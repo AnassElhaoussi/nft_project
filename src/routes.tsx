5343c61a0a46c0edae7d2ed510e23806f9a01168
anasselhaoussi <anous.elhaoussi@gmail.com>
2023-11-06 20:15:25 +0100

    adding main component

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Leaderboards from "./Pages/Leaderboards";
 import App from "./Pages/App";
+import {Link} from "react-router-dom"
+
+export const Main = () => {
+    return (
+        <div>
+            <Link to="/marketplace">Go to marketplace</Link>
+            <Link to="/leaderboards">Go to leaderboards</Link>
+        </div>
+    )
+}
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Main />} />
         <Route path="/marketplace" element={<App />} />
         <Route path="/leaderboards" element={<Leaderboards />} />
         <Route />
